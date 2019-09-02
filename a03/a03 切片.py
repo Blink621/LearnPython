003631d7dcdf67c494e3ed109eb575d84250ca2d
Blink621 <54697182+Blink621@users.noreply.github.com>
2019-09-02 15:50:55 +0800

    Create a03 切片.py

---
+++ a03/a03 切片.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
+
+def trim(s):
+    a=len(s)
+    b=c=0
+    for i in range(a):
+        if s[i]==' ':
+            b=b+1
+        else: 
+            break
+    for i in range(a):
+        if s[-i]==' ':
+            c=c+1
+        else: 
+            break
+    s=s[b:a-c+1]
+    return s
+
+d=trim('  hello  world  ')
+print(trim('  hello  world  '),len(d))
+print(trim('   hello  world    '))
+print(trim('    '))
