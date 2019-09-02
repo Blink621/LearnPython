d56a6ee0d6da17cca054ee4b66aa8cc1c3e7a6d7
Blink621 <54697182+Blink621@users.noreply.github.com>
2019-09-02 12:24:00 +0800

    Create a02 递归函数.py

---
+++ a02 函数/a02 递归函数.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+……
+请编写move(n, a, b, c)函数，它接收参数n，表示3个柱子A、B、C中第1个柱子A的盘子数量，然后打印出把所有盘子从A借助B移动到C的方法
+……
+
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '-->', c)
+    else:
+        move(n-1,a,c,b)
+        move(1,a,b,c)
+        move(n-1,b,a,c)
+    return
+
+
+move(4, 'A', 'B', 'C')
